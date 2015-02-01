d8a01e417172a661e83de473c5f43c899d384cd4
Simo-Pekka Kerkelä <simopekka1990@gmail.com>
2015-02-01 18:10:08 +0200

    Use Reactjs

--- resources/public/js/mmh.js
+++ resources/public/js/mmh.js
@@ -1,3 +1,17 @@
 /**
  * Created by spkerkela on 01/02/15.
  */
+
+var CommentBox = React.createClass({displayName: 'CommentBox',
+    render: function() {
+        return (
+            React.createElement('h1', {className: "commentBox"},
+                "Welcome to MMH."
+            )
+        );
+    }
+});
+React.render(
+    React.createElement(CommentBox, null),
+    document.getElementById('content')
+);
